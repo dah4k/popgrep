a2f122c036def2a928f38fe0884d44c96562e0ad
dah4k <84741290+dah4k@users.noreply.github.com>
2024-11-11 22:28:39 -0500

    Fix popgrep1.pl to correctly print the matched lines
    The historical and working Perl code was probably more like:
    
        @popstates = qw(CO ON MI WI MN);
        LINE: while (<>) {
            for $state (@popstates) {
                if (/\b$state\b/) {
                    print; next LINE;
                }
            }
        }
    
    This `while(<>)` loop is very similar to the one in popgrep version 3.
    At some point in the past, the `while(<>)` loop likely got refactored
    into the `while(defined($line = <>))` loop.  This newer loop has an
    uninitialized bug when trying to print the matched line.
    
        % ./popgrep1.pl < popgrep1.pl
        Use of uninitialized value $_ in print at ./popgrep1.pl line 8, <> line 6.
    
    The fixed Perl version 1 correctly prints the expected matched line.
    
        % ./popgrep1.pl < popgrep1.pl
        @popstates = qw(CO ON MI WI MN);
    
        % ./popgrep1.pl < popgrep1.py
        popstates = ['CO', 'ON', 'MI', 'WI', 'MN']

--- popgrep1.pl
+++ popgrep1.pl
@@ -1,11 +1,11 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
 # popgrep1 - grep for abbreviations of places that say "pop"
 # version 1: slow but obvious way
 @popstates = qw(CO ON MI WI MN);
 LINE: while (defined($line = <>)) {
     for $state (@popstates) {
         if ($line =~ /\b$state\b/) {
-            print; next LINE;
+            print $line; next LINE;
         }
     }
 }
